9c91bc2c192ec13dc222560342b97635346fbc54
rpedde <ron@pedde.com>
2014-05-21 19:18:22 -0500

    pretty

--- src/selfcheck.c
+++ src/selfcheck.c
@@ -2,9 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define COLOR_RED    "\x1b[31m"
+#define COLOR_GREEN  "\x1b[32m"
+#define COLOR_RESET  "\x1b[0m"
+
 #define MAX(a, b) (a) > (b) ? (a) : (b)
 
 #include "test-definitions.h"
+
 
 typedef struct test_t {
     char *test_name;
@@ -20,18 +25,18 @@ int main(int argc, char *argv[]) {
     int success=1;
     int result;
 
-    printf("Testing.\n--------------------------------------------------------------------\n");
+    printf("Testing.\n------------------------------------------------\n");
 
     while(current->test_fn) {
         printf("%s%*s: ", current->test_name, MAX(1, 40 - strlen(current->test_name)), " ");
         result = current->test_fn(NULL);
-        printf("%s\n", result ? "SUCCESS" : "FAIL");
+        printf("%s\n" COLOR_RESET, result ? COLOR_GREEN "Ok": COLOR_RED "Error");
         success |= result;
         current++;
     }
 
-    printf("--------------------------------------------------------------------\n");
-    printf("Result: %s\n", success ? "SUCCESS" : "FAIL");
+    printf("------------------------------------------------\n");
+    printf("Result: %s\n" COLOR_RESET, success ? COLOR_GREEN "Ok" : COLOR_RED "Error");
 
     exit(!success);
 }
