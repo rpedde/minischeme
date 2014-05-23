bb7fea0dbf467c1c716f447ee5e225921d54c522
William Kelly <the.william.kelly@gmail.com>
2014-05-22 21:11:11 -0500

    Add list parser tests

--- src/test_parser.c
+++ src/test_parser.c
@@ -41,3 +41,48 @@ int test_float_parsing(void *scaffold) {
     return 1;
 }
 
+int test_list_parsing(void *scaffold) {
+    lisp_value_t *result;
+    result = lisp_parse_string("()");
+    assert(result == NULL); // this might be its own lisp type?
+
+    result = lisp_parse_string("(1)");
+    assert(result->type == l_pair);
+    assert(L_CAR(result)->type == l_int);
+    assert(L_INT(L_CAR(result)) == 1);
+    assert(L_CDR(result) == NULL);
+
+    result = lisp_parse_string("(1 . ())");
+    assert(result->type == l_pair);
+    assert(L_CAR(result)->type == l_int);
+    assert(L_INT(L_CAR(result)) == 1);
+    assert(L_CDR(result) == NULL);
+
+    result = lisp_parse_string("(1 . 1)");
+    assert(result->type == l_pair);
+    assert(L_CAR(result)->type == l_int);
+    assert(L_CDR(result)->type == l_int);
+    assert(L_INT(L_CAR(result)) == 1);
+    assert(L_INT(L_CDR(result)) == 1);
+
+    result = lisp_parse_string("(1 1)");
+    assert(result->type == l_pair);
+    assert(L_CAR(result)->type == l_int);
+    assert(L_CDR(result)->type == l_pair)
+    assert(L_CAR(L_CDR(result))->type == l_int);;
+    assert(L_CDR(L_CDR(result)) == NULL);
+    assert(L_INT(L_CAR(result)) == 1);
+    assert(L_INT(L_CAR(L_CDR(result))) == 1);
+
+
+    result = lisp_parse_string("(1 . (1 . ()))");
+    assert(result->type == l_pair);
+    assert(L_CAR(result)->type == l_int);
+    assert(L_CDR(result)->type == l_pair)
+    assert(L_CAR(L_CDR(result))->type == l_int);;
+    assert(L_CDR(L_CDR(result)) == NULL);
+    assert(L_INT(L_CAR(result)) == 1);
+    assert(L_INT(L_CAR(L_CDR(result))) == 1);
+
+    return 1;
+}
