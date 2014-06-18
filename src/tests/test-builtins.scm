fe710dc703a6a7f7053b9001803e1c3417f5df38
William Kelly <the.william.kelly@gmail.com>
2014-06-18 18:42:19 -0500

    Add tests for cons behavior with nil
    Currently, the testing framework is segfaulting on these tests, so
    I've commented them out.

--- src/tests/test-builtins.scm
+++ src/tests/test-builtins.scm
@@ -6,5 +6,9 @@
 (define test-cdr1 (lambda () (assert (equal? '(2 3) (cdr '(1 2 3))))))
 (define test-cdr2 (lambda () (assert (equal? 2 (cdr '(1 . 2))))))
 
-(define test-cons1 (lambda () (assert (equal? '(1 2 3) (cons '1 '(2 3))))))
-(define test-cons2 (lambda () (assert (equal? '(1 . 2) (cons '1 '2)))))
+(define test-cons1 (lambda () (assert (equal? '(1 2 3) (cons 1 '(2 3))))))
+(define test-cons2 (lambda () (assert (equal? '(1 . 2) (cons 1 2)))))
+;; (define test-cons3 (lambda () (assert (equal? '(() . 0) (cons '() 0)))))
+;; (define test-cons4 (lambda () (assert (equal? '(0) (cons 0 '())))))
+;; (define test-cons5
+;;   (lambda () (assert (equal? '(()) (cons '() '())))))
